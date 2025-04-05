66f006af9e736f52f4d231d36e85c11156c80e3b
Ether <shvedov_es@surgu.ru>
2025-04-05 15:32:04 +0500

    matplotlib simple plot

---
+++ plotlib1.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Data for plotting
+t = np.arange(0.0, 2.0, 0.01)
+print(t)
+s = 1 + np.sin(2 * np.pi * t)
+print(s)
+fig, ax = plt.subplots()
+print(fig)
+print(ax)
+ax.plot(t, s)
+
+ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+       title='About as simple as it gets, folks')
+# ax.grid()
+
+fig.savefig("test.png")
+plt.show()
